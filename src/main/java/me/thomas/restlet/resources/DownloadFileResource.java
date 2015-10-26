4db452679d8e5e94a4764a7e6df7d38591e7c83c
Thomas Zhao <zhao.xshg47@gmail.com>
2015-10-26 10:10:51 +0800

    根据参数选择下载的文件

--- src/main/java/me/thomas/restlet/resources/DownloadFileResource.java
+++ src/main/java/me/thomas/restlet/resources/DownloadFileResource.java
@@ -1,28 +1,22 @@
 package me.thomas.restlet.resources;
 
-import me.thomas.restlet.entity.UploadFile;
-import org.apache.commons.fileupload.FileItem;
-import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.restlet.data.Disposition;
 import org.restlet.data.MediaType;
-import org.restlet.ext.fileupload.RestletFileUpload;
 import org.restlet.representation.FileRepresentation;
 import org.restlet.representation.Representation;
 import org.restlet.resource.Get;
-import org.restlet.resource.Post;
 import org.restlet.resource.ServerResource;
 
 import java.io.File;
-import java.util.List;
 
 /**
  * Created by thomas on 2014/12/26.
  */
 public class DownloadFileResource extends ServerResource {
 
-    @Get(value = "pdf")
+    @Get(value = "?pdf")
     public Representation downloadPdf(Representation entity) throws Exception {
-        File file = new File("/home/thomas/Documents/hftp.pdf");
+        File file = new File("C:\\Users\\Public\\Documents\\letter.pdf");
         Representation fileRepresentation = new FileRepresentation(file, MediaType.APPLICATION_PDF);
         fileRepresentation.setDisposition(new Disposition(Disposition.TYPE_ATTACHMENT));
         fileRepresentation.getDisposition().setFilename(file.getName());
@@ -30,9 +24,9 @@ public class DownloadFileResource extends ServerResource {
         return fileRepresentation;
     }
 
-    @Get(value = "jpg")
+    @Get(value = "?jpg")
     public Representation downloadJpg(Representation entity) throws Exception {
-        File file = new File("/home/thomas/Pictures/t0125e533c809109624.jpg");
+        File file = new File("C:\\Users\\Public\\Pictures\\Sample Pictures\\829BJDH633AN.jpg");
         Representation fileRepresentation = new FileRepresentation(file, MediaType.IMAGE_JPEG);
         fileRepresentation.setDisposition(new Disposition(Disposition.TYPE_ATTACHMENT));
         fileRepresentation.getDisposition().setFilename(file.getName());
--- src/main/java/me/thomas/restlet/resources/UploadFileResource.java
+++ src/main/java/me/thomas/restlet/resources/UploadFileResource.java
@@ -2,14 +2,10 @@ package me.thomas.restlet.resources;
 
 import me.thomas.restlet.entity.UploadFile;
 import org.apache.commons.fileupload.FileItem;
-import org.apache.commons.fileupload.FileUploadException;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
-import org.restlet.data.Disposition;
 import org.restlet.data.MediaType;
 import org.restlet.ext.fileupload.RestletFileUpload;
-import org.restlet.representation.FileRepresentation;
 import org.restlet.representation.Representation;
-import org.restlet.resource.Get;
 import org.restlet.resource.Post;
 import org.restlet.resource.ServerResource;
 
